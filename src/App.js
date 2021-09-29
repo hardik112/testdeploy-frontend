e1cd6e90f87ea03aa76725c63d6a7cc6949c94a9
Hardik Soni <hardik.soni@thegatewaydigital.com>
2021-09-29 21:32:42 +0530

    Har Har Mahadev

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Har Har Mahadev, Need to check for S3 Public access
         </p>
         <a
           className="App-link"
